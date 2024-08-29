6116784ae278a34e7fffc5ab8d27899a853dd84f
M.Z <zeynalov@gmail.com>
2024-08-29 20:58:44 +0400

    draft project

---
+++ Hl7SampleApplication/Controllers/Hl7Controller.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hl7SampleApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Hl7Controller : ControllerBase
+    {
+        private readonly ILogger<Hl7Controller> _logger;
+
+        public Hl7Controller(ILogger<Hl7Controller> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost(Name = "SendAppointment")]
+        public async Task<IActionResult> SendAppointment()
+        {
+            var a = 1; 
+            
+            if (a == 1)
+                return NotFound(); 
+            
+            await Task.CompletedTask;
+
+            return Ok("some response..."); 
+        }
+    }
+}
