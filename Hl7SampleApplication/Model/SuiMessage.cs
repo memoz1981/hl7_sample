7d2fc4f0180ea3f44f1eefc6f7f74aefc7cac30e
M.Z <zeynalov@gmail.com>
2024-08-29 21:59:33 +0400

    created draft method/class for encoding sui s12

--- Hl7SampleApplication/Model/SuiMessage.cs
+++ Hl7SampleApplication/Model/SuiMessage.cs
@@ -1,6 +1,24 @@
 ﻿namespace Hl7SampleApplication.Model
 {
-    public class SuiMessage
+    public record SuiMessage
     {
+        public MshSegment Msh { get; set; }
+    }
+
+    public record MshSegment
+    {
+        public string MsgType { get; set; }
+        public string TriggerEvent { get; set; }
+        public string MsgStructure { get; set; }
+        public string FieldSep { get; set; }
+        public string SendApp { get; set; }
+        public string SendFac { get; set; }
+        public string RecvApp { get; set; }
+        public string RecvFac { get; set; }
+        public string EncChars { get; set; }
+        public string Version { get; set; }
+        public DateTime MsgDateTime { get; set; }
+        public string MsgCtrlId { get; set; }
+        public string ProcId { get; set; }
     }
 }
--- Hl7SampleApplication/Services/Encoder.cs
+++ Hl7SampleApplication/Services/Encoder.cs
@@ -1,10 +1,70 @@
 ﻿using Hl7SampleApplication.Model;
+using NHapi.Base.Parser;
+using NHapi.Model.V281.Message;
 
 namespace Hl7SampleApplication.Services;
 
 public class Encoder : IEncoder
 {
     public string Encode(SuiMessage message)
+    {
+        var parser = new PipeParser();
+        var qry = new NHapi.Model.V281.Message.SIU_S12();
+
+        AssignMsh(message, qry);
+        AssignSCH(message, qry);
+        AssignPID(message, qry);
+        AssignRGS(message, qry);
+        AssignAIG(message, qry);
+        AssignAIL(message, qry);
+        AssignAIP(message, qry);
+
+        //additional fields/properties here...
+
+        return parser.Encode(qry);
+    }
+
+    private void AssignMsh(SuiMessage message, NHapi.Model.V281.Message.SIU_S12 qry)
+    {
+        qry.MSH.MessageType.TriggerEvent.Value = message.Msh.TriggerEvent;
+        qry.MSH.MessageType.MessageStructure.Value = message.Msh.MsgStructure;
+        qry.MSH.FieldSeparator.Value = message.Msh.FieldSep;
+        qry.MSH.SendingApplication.NamespaceID.Value = message.Msh.SendApp;
+        qry.MSH.SendingFacility.NamespaceID.Value = message.Msh.SendFac;
+        qry.MSH.ReceivingApplication.NamespaceID.Value = message.Msh.RecvApp;
+        qry.MSH.ReceivingFacility.NamespaceID.Value = message.Msh.RecvFac;
+        qry.MSH.EncodingCharacters.Value = message.Msh.EncChars;
+        qry.MSH.VersionID.VersionID.Value = message.Msh.Version;
+
+        qry.MSH.MessageControlID.Value = message.Msh.MsgCtrlId;
+        qry.MSH.ProcessingID.ProcessingID.Value = message.Msh.ProcId;
+    }
+    private void AssignAIP(SuiMessage message, SIU_S12 qry)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void AssignAIL(SuiMessage message, SIU_S12 qry)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void AssignAIG(SuiMessage message, SIU_S12 qry)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void AssignRGS(SuiMessage message, SIU_S12 qry)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void AssignPID(SuiMessage message, SIU_S12 qry)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void AssignSCH(SuiMessage message, NHapi.Model.V281.Message.SIU_S12 qry)
     {
         throw new NotImplementedException();
     }
